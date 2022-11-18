df2d0478e4b074db6c23d934dbd942ae140ffb96
rppicomidi <rppicomidi@gmail.com>
2022-11-18 15:13:56 -0800

    support latest MIDI UART library

--- midi_uart_lib_config.h
+++ midi_uart_lib_config.h
@@ -30,12 +30,16 @@
 #ifndef MIDI_UART_LIB_CONFIG_H
 #define MIDI_UART_LIB_CONFIG_H
 
-// Legal values for SERIAL_PORT_MIDI_NUM_UARTS:
+// Legal values for MIDI_UART_LIB_NUM_UARTS:
 // 1: assigns RP2040 UART1 to MIDI and does not assign UART0
 // 2: assigns both RP2040 UART0 and UART1 to MIDI
 // Any other value will cause a compiler error
 // Note if you use both serial ports for MIDI then neither will
 // be available for debugging
-#define SERIAL_PORT_MIDI_NUM_UARTS 1
+#define MIDI_UART_LIB_NUM_UARTS 1
+
+// The standard MIDI UART baud rate is 31250. If you want to do
+// something special with the MIDI UART LIB, you can change this
+#define MIDI_UART_LIB_BAUD_RATE 31250
 
 #endif
